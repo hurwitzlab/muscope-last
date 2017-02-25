ac4416b1ec7de5e0ed07b8bd358e50af8607fafe
Joshua Lynch <jklynch@email.arizona.edu>
2017-02-25 20:57:22 +0000

    change blast to last

--- stampede/test.sh
+++ stampede/test.sh
@@ -5,14 +5,14 @@
 #SBATCH -n 1
 #SBATCH -t 00:30:00
 #SBATCH -p development
-#SBATCH -J mublast
+#SBATCH -J mulast
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user jklynch@email.arizona.edu
 
-OUT_DIR="$SCRATCH/muscope-blast/test"
+OUT_DIR="$SCRATCH/muscope-last/test"
 
 if [[ -d $OUT_DIR ]]; then
   rm -rf $OUT_DIR
 fi
 
-run.sh -q "$SCRATCH/muscope-blast/test.fa" -o $OUT_DIR
+run.sh -q "$SCRATCH/muscope-last/test.fa" -o $OUT_DIR
