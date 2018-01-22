ed2eb96c05a93bc7d9a76640f4b69d4f8dc34b91
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-22 15:54:46 -0500

    updates for stampede2

--- setup/build_last_dbs.sh
+++ setup/build_last_dbs.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
-#
-# Simple SLURM script for submitting multiple serial
-# jobs (e.g. parametric studies) using a script wrapper
-# to launch the jobs.
-#
-# To use, build the launcher executable and your
-# serial application(s) and place them in your WORKDIR
-# directory.  Then, edit the CONTROL_FILE to specify
-# each executable per process.
-#-------------------------------------------------------
-#-------------------------------------------------------
-#
-#         <------ Setup Parameters ------>
-#
-#SBATCH -J build-last-dbs
-#SBATCH -N 1
-#SBATCH -n 2
-#SBATCH -p development
-#SBATCH -e build-last-dbs.e%j
-#SBATCH -o build-last-dbs.o%j
-#SBATCH -t 00:30:00
-#          <------ Account String ----->
-# <--- (Use this ONLY if you have MULTIPLE accounts) --->
-#SBATCH -A TG-ASC160037
-#------------------------------------------------------
 
-#module load launcher
+#SBATCH -A iPlant-Collabs
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 02:00:00
+#SBATCH -p skx-normal
+#SBATCH -J build-last-dbs
+#SBATCH --mail-type BEGIN,END,FAIL
+#SBATCH --mail-user jklynch@email.arizona.edu
+
+module load launcher
 
 export LAUNCHER_PLUGIN_DIR=$TACC_LAUNCHER_DIR/plugins
 export LAUNCHER_RMI=SLURM
-export LAUNCHER_JOB_FILE=$WORK/cyverse-apps/muscope-last/setup/jobfile
+export LAUNCHER_JOB_FILE=jobfile
 
 $TACC_LAUNCHER_DIR/paramrun
