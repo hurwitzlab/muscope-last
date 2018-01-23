b15e28b41bbdfe32fe1638d74d50c09ab7bd6309
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-22 21:30:52 -0500

    reduce time

--- setup/build_genes_last_db.sh
+++ setup/build_genes_last_db.sh
@@ -5,7 +5,7 @@
 #SBATCH -p skx-normal
 #SBATCH -e build-genes-last-dbs.e%j
 #SBATCH -o build-genes-last-dbs.o%j
-#SBATCH -t 12:00:00
+#SBATCH -t 06:00:00
 #SBATCH -A iPlant-Collabs
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user jklynch@email.arizona.edu
@@ -16,6 +16,6 @@ OHANA_HOT_DIR=/work/05066/imicrobe/iplantc.org/data/ohana/HOT
 LAST_DB_DIR=/work/05066/imicrobe/iplantc.org/data/ohana/last
 
 ls -l $LAST_DB_DIR
-rm $LAST_DB_DIR/HOT_genes*
+rm -f $LAST_DB_DIR/HOT_genes*
 singularity exec ../stampede2/muscope-last.img lastdb -cR01 -v $LAST_DB_DIR/HOT_genes $OHANA_HOT_DIR/HOT*/genes.fna
 ls -l $LAST_DB_DIR
--- setup/build_proteins_last_db.sh
+++ setup/build_proteins_last_db.sh
@@ -5,7 +5,7 @@
 #SBATCH -p skx-normal
 #SBATCH -e build-proteins-last-dbs.e%j
 #SBATCH -o build-proteins-last-dbs.o%j
-#SBATCH -t 12:00:00
+#SBATCH -t 06:00:00
 #SBATCH -A iPlant-Collabs
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user jklynch@email.arizona.edu
@@ -17,6 +17,6 @@ OHANA_HOT_DIR=/work/05066/imicrobe/iplantc.org/data/ohana/HOT
 LAST_DB_DIR=/work/05066/imicrobe/iplantc.org/data/ohana/last
 
 ls -l $LAST_DB_DIR
-rm $LAST_DB_DIR/HOT_proteins*
+rm -f $LAST_DB_DIR/HOT_proteins*
 singularity exec ../stampede2/muscope-last.img lastdb -cR01 -p -v $LAST_DB_DIR/HOT_proteins $OHANA_HOT_DIR/HOT*/proteins.faa
 ls -l $LAST_DB_DIR
