bfd62fea1f213876478d8488f84e77d3cf824a52
Joshua Lynch <jklynch@email.arizona.edu>
2018-01-25 09:22:27 -0500

    get the path to the script right remove some unused environment variables

--- setup/build_sqlite_seq_dbs.sh
+++ setup/build_sqlite_seq_dbs.sh
@@ -14,7 +14,6 @@ module load tacc-singularity
 
 export LAUNCHER_DIR=${TACC_LAUNCHER_DIR}
 
-#HOT_DIR=/work/03137/kyclark/ohana/HOT
 IMICROBE_DATA_DIR=/work/05066/imicrobe/iplantc.org/data
 HOT_DIR=$IMICROBE_DATA_DIR/ohana/HOT
 OUT_DIR=$IMICROBE_DATA_DIR/ohana/last/seq_db
@@ -30,7 +29,7 @@ find $HOT_DIR -size +0c -name proteins.faa >> $SEQ_FILES
 while read FILE; do
   BASENAME=$(basename $FILE '.tab')
   echo "Building SQLite db with $FILE"
-  echo "singularity exec ../stampede2/muscope-last.img python3 /scripts/build_sqlite_seq_db.py -i \"$FILE\" -o \"${OUT_DIR}\"" >> $SQLITE_DB_JOBS
+  echo "singularity exec ../stampede2/muscope-last.img python3 scripts/build_sqlite_seq_db.py -i \"$FILE\" -o \"${OUT_DIR}\"" >> $SQLITE_DB_JOBS
 done < $SEQ_FILES
 
 echo "Starting launcher"
@@ -39,23 +38,14 @@ echo "  SLURM_NTASKS=$SLURM_NTASKS"
 echo "  SLURM_JOB_CPUS_PER_NODE=$SLURM_JOB_CPUS_PER_NODE"
 echo "  SLURM_TASKS_PER_NODE=$SLURM_TASKS_PER_NODE"
 
-#BIN=$( cd "$( dirname "%0" )" $$ pwd)
-#echo " BIN dir: $BIN"
-
-#export LAUNCHER_DIR="$HOME/src/launcher"
 export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
-#export LAUNCHER_WORKDIR=$BIN
 export LAUNCHER_RMI=SLURM
-#export LAUNCHER_NHOSTS=$SLURM_JOB_NUM_NODES
-#export LAUNCHER_NPROCS=$SLURM_TASKS_PER_NODE
 # 48 cores per SKX node
 export LAUNCHER_PPN=48
 export LAUNCHER_SCHED=dynamic
 
 export LAUNCHER_JOB_FILE=$SQLITE_DB_JOBS
-#export LAUNCHER_NJOBS=$(wc -l < $SQLITE_DB_JOBS)
 
-#echo "  LAUNCHER_NJOBS=$LAUNCHER_NJOBS"
 echo "  LAUNCHER_NHOSTS=$LAUNCHER_NHOSTS"
 echo "  LAUNCHER_NPROCS=$LAUNCHER_NPROCS"
 echo "  LAUNCHER_PPN=$LAUNCHER_PPN"
